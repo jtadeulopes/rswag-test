07238fcde6d7722eb7f089d8636ba903beb64a9d
Jésus Lopes <jesus.tadeu@redealumni.com>
2021-09-15 16:31:36 -0300

    Update readme

---
+++ app/models/parking.rb
@@ -0,0 +1,15 @@
+class Parking < ApplicationRecord
+  PLATE_REGEX = /\A[A-Z]{3}-[0-9]{4}\z/
+
+  validates :plate,
+    format: { with: PLATE_REGEX, message: 'invalid plate', allow_blank: true },
+    presence: true
+
+  def pay!
+    update(paid: true, paid_at: Time.current) unless paid?
+  end
+
+  def ticket
+    "PK-#{id.to_s.last(4).upcase}"
+  end
+end
