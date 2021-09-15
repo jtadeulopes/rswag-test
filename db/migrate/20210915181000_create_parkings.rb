eed2967affe8cf65957825d7d9cb5b4f0a1cd431
Jésus Lopes <jesus.tadeu@redealumni.com>
2021-09-15 15:12:26 -0300

    Add parking model

---
+++ db/migrate/20210915181000_create_parkings.rb
@@ -0,0 +1,10 @@
+class CreateParkings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :parkings do |t|
+      t.string :plate
+      t.boolean :paid, default: false
+
+      t.timestamps
+    end
+  end
+end
